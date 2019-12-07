5eb00c9ffa5d7f4e7f7f6cb371df1f0c17dc5e1a
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-12-06 16:57:32 -0800

    FIX - missing error-handling from the original app

--- src/components/pages/IssuesListPage.tsx
+++ src/components/pages/IssuesListPage.tsx
@@ -5,10 +5,17 @@ import { IssuesPageHeader } from './IssuesListLib/IssuesPageHeader'
 import { IssuesList } from './IssuesListLib/IssuesList'
 import { IssuePagination } from './IssuesListLib/IssuePagination'
 
-export const IssuesListPage = () =>
-  <div id="issue-list-page">
-    <RepoSearchForm />
-    <IssuesPageHeader />
-    {useIssues().loading ? <h3>Loading...</h3> : <IssuesList />}
-    <IssuePagination />
-  </div>
+export const IssuesListPage = () => {
+  const { loading, error } = useIssues()
+  return error
+    ? <div>
+      <h1>Something went wrong...</h1>
+      <div>{error.toString()}</div>
+    </div>
+    : <div id="issue-list-page">
+      <RepoSearchForm />
+      <IssuesPageHeader />
+      {loading ? <h3>Loading...</h3> : <IssuesList />}
+      <IssuePagination />
+    </div>
+}
