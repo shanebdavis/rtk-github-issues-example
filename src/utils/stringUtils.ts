cd4fa26e2ac5b772223ada3d81de6a6559a75f9f
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2020-06-19 10:44:44 -0700

    patch/fix: fixed 'shorten' failing when text is null as opposed to undefined

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -6,6 +6,7 @@ export function insertMentionLinks(markdown: string) {
 }
 
 export function shorten(text = '', maxLength = 140) {
+  if (text == null) return '';
   // Normalize newlines
   let cleanText = text.replace(/\\r\\n/g, '\n')
 
